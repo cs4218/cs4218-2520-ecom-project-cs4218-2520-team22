212166fc868233be610da2cb284e923b89aae7b6
ChocoRagdoll <E0960356@u.nus.edu>
2026-02-22 14:47:10 +0800

    Add credits

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
+    // fixed config issue for react testing, Daniel Lai, A0192327A
     ['@babel/preset-react', { runtime: 'automatic' }],
   ]
 };
