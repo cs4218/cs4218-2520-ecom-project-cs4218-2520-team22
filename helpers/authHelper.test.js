4a19db939c3daf4ba8522ae04b4bdc8662f6a224
Daniel Lai <e0360577@u.nus.edu>
2026-03-14 14:50:27 +0800

    Add sanity check for encryption library

--- helpers/authHelper.test.js
+++ helpers/authHelper.test.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import { hashPassword, comparePassword } from "./authHelper";
+import { before } from "node:test";
 
 jest.mock('bcrypt');
 
@@ -21,6 +22,32 @@ describe("authHelper Component", () => {
 
       expect(console.log).toHaveBeenCalledWith(mockError);
       expect(result).toBeUndefined();
+    });
+  });
+
+  describe("Integration Testing", () => {
+    beforeAll(() => {
+      jest.unmock('bcrypt');
+    });
+    
+    it("hash should not return plaintext, and should compare consistently with the same plaintext", async () => {
+      const plainPassword = "mysecretpassword";
+      const hashedPassword = await hashPassword(plainPassword);
+      
+      const isMatch = await comparePassword(plainPassword, hashedPassword);
+      
+      expect(plainPassword).not.toBe(hashedPassword); // Ensure hashing is working
+      expect(isMatch).toBe(true);
+    });
+
+    it("comparePassword should return false for different plaintexts", async () => {
+      const plainPassword = "mysecretpassword";
+      const differentPassword = "anotherpassword";
+      const hashedPassword = await hashPassword(plainPassword);
+      
+      const isMatch = await comparePassword(differentPassword, hashedPassword); 
+      
+      expect(isMatch).toBe(false);
     });
   });
 });
