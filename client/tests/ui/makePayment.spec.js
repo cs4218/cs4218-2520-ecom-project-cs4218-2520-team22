f3dc8dfedf16e5a76d8afd502d3e3fe20db54056
limjunxian1 <junxianlim@outlook.com>
2026-03-19 23:17:12 +0800

    update e2e tests

--- client/tests/ui/makePayment.spec.js
+++ client/tests/ui/makePayment.spec.js
@@ -1,35 +1,6 @@
 import { test, expect } from '@playwright/test';
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import userModel from "../models/userModel";
-import orderModel from '../models/orderModel';
-
-dotenv.config();
 
 const BASE_URL = "http://localhost:3000";
-
-let user
-
-test.beforeEach(async ({ page }) => {
-    await mongoose.connect(process.env.MONGO_URL);
-
-    const hashedPassword = "$2b$10$//wWsN./fEX1WiipH57HG.SAwgkYv1MRrPSkpXM38Dy5seOEhCoUy";
-
-    user = await new userModel({
-        name: "Test",
-        email: "test@yahoo.com",
-        phone: "81234567",
-        address: "test",
-        password: hashedPassword,
-        answer: "password is cs4218@test.com",
-    }).save(); 
-});
-
-test.afterEach(async () => {
-    await userModel.deleteMany({ email: "test@yahoo.com" });
-    await orderModel.deleteMany({ buyer: user._id });
-    await mongoose.disconnect();
-});
 
 test("Make payment successfully for items added to cart", async({page}) => {
     await page.goto(BASE_URL);
@@ -42,7 +13,7 @@ test("Make payment successfully for items added to cart", async({page}) => {
 
     // Login from Cart Page
     await page.getByRole('button', { name: 'Please login to checkout' }).click();
-    await page.getByRole("textbox", { name: "Enter Your Email" }).fill("test@yahoo.com");
+    await page.getByRole("textbox", { name: "Enter Your Email" }).fill("cs4218@test.com"); // Use test account
     await page.getByRole("textbox", { name: "Enter Your Password" }).fill("cs4218@test.com");
     await page.getByRole("button", { name: "Login" }).click();
 
--- client/tests/ui/updateUserAddress.spec.js
+++ client/tests/ui/updateUserAddress.spec.js
@@ -1,31 +1,6 @@
 import { test, expect } from '@playwright/test';
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import userModel from "../models/userModel";
-
-dotenv.config();
 
 const BASE_URL = "http://localhost:3000";
-
-test.beforeEach(async ({ page }) => {
-    await mongoose.connect(process.env.MONGO_URL);
-
-    const hashedPassword = "$2b$10$//wWsN./fEX1WiipH57HG.SAwgkYv1MRrPSkpXM38Dy5seOEhCoUy";
-
-    await new userModel({
-        name: "Test",
-        email: "test@email.com",
-        phone: "81234567",
-        address: "test",
-        password: hashedPassword,
-        answer: "password is cs4218@test.com",
-    }).save(); 
-});
-
-test.afterEach(async () => {
-    await userModel.deleteMany({ email: "test@email.com" });
-    await mongoose.disconnect();
-});
 
 test('Update user address successfully while navigating from Cart Page -> Login Page -> Profile Page', async ({ page }) => {
     await page.goto(BASE_URL);
@@ -35,7 +10,7 @@ test('Update user address successfully while navigating from Cart Page -> Login 
     
     // Login from Cart Page
     await page.getByRole('button', { name: 'Please login to checkout' }).click();
-    await page.getByRole("textbox", { name: "Enter Your Email" }).fill("test@email.com");
+    await page.getByRole("textbox", { name: "Enter Your Email" }).fill("cs4218@test.com"); // Use test account
     await page.getByRole("textbox", { name: "Enter Your Password" }).fill("cs4218@test.com");
     await page.getByRole("button", { name: "Login" }).click();
 
