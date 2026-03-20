9d53c0a1d7a09f3865bd755b1fff3b5da2b74ac3
Daniel Lai <e0360577@u.nus.edu>
2026-03-20 17:44:50 +0800

    Undo change

--- client/src/components/Routes/AdminRoute.js
+++ client/src/components/Routes/AdminRoute.js
@@ -10,14 +10,10 @@ export default function AdminRoute(){
 
     useEffect(()=> {
         const authCheck = async() => {
-            try {
-                const res = await axios.get("/api/v1/auth/admin-auth");
-                if(res.data.ok){
-                    setOk(true);
-                } else {
-                    setOk(false);
-                }
-            } catch (error) {
+            const res = await axios.get("/api/v1/auth/admin-auth");
+            if(res.data.ok){
+                setOk(true);
+            } else {
                 setOk(false);
             }
         };
