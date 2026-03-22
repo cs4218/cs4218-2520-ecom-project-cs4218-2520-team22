216e093d56eca6cd04c6939e31b3c439ba748929
Daniel Lai <e0360577@u.nus.edu>
2026-03-22 18:38:04 +0800

    Fix config issue introduced by merge

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./e2e",
+  testDir: ".",
   timeout: 45000,
   retries: process.env.CI ? 1 : 0,
   use: {
